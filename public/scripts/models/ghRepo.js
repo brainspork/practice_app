a15e0d51d6553b466a96bad2403990a0abe0a0c3
Anthony Konschuh <silv.amee@gmail.com>
2017-06-29 11:26:36 -0700

    Adds api call out for my repos
    Adds an IIFE that calls out to the git hub api and returns an array of my repos. Adds github token file and git ignores it

---
+++ public/scripts/models/ghRepo.js
@@ -0,0 +1,22 @@
+'use strict';
+var app = app || {};
+
+
+(function(module) {
+  const repo = {};
+
+  repo.all = [];
+
+  repo.fetchData = (callback) => {
+    $.ajax({
+      url: 'https://api.github.com/user/repos',
+      headers: token,
+      success: (data, message) => {
+        repo.all = data;
+        console.log(message);
+      }
+    })
+  }
+
+  module.repo = repo;
+}(app));
